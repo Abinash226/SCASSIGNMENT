f522010ae99d42e11a14f0df643146a6d908615f
abimohanty <abinashm226@gmail.com>
2019-06-21 11:35:44 +0530

    'test'

--- src/main/java/com/sc/Employee/Controller/EmployeeController.java
+++ src/main/java/com/sc/Employee/Controller/EmployeeController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +22,7 @@ import io.swagger.annotations.ApiParam;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/employee")
 @CrossOrigin("*")
 @Api("Set of endpoints for Creating, Retrieving, Updating and Deleting of Employees.")
 public class EmployeeController {
